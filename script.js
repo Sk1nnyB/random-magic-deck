ba5073616e3fb4a87e90e4c277eaf6c7d04e4529
Ben Barrow <114662236+Sk1nnyB@users.noreply.github.com>
2024-02-11 20:21:08 +0000

    Add new effect generator

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-function getRandomDeck(decks) {
-    var deck = Math.floor(Math.random() * decks.length);
-    return decks[deck];
-}
+function getRandomEntry(array) {
+    var entry = Math.floor(Math.random() * array.length);
+    return array[entry];
+};
 
 var decks_1 = ["Alesha, Who Smiles at Death"];
 var decks_2 = ["Ranar the Everwatching"];
@@ -16,7 +16,7 @@ var decks_9 = ["Tuvasa", "The Scarab God", "Meren", "Lord Windgrace"];
 var decks_10 = ["Miirym", "Scion", "Yarok", "Veyran"];
 var decks_11 = ["Jhoira", "Go outside."];
 
-
+var effects = ["Target player gets you a drink"];
 
 document.getElementById('numberForm').addEventListener('submit', function(event) {
     event.preventDefault();
@@ -25,24 +25,35 @@ document.getElementById('numberForm').addEventListener('submit', function(event)
     var inputNumber = document.getElementById('powerLevel').value;
 
     var responses = {
-        1: getRandomDeck(decks_1),
-        2: getRandomDeck(decks_2),
-        3: getRandomDeck(decks_3),
-        4: getRandomDeck(decks_4),
-        5: getRandomDeck(decks_5),
-        6: getRandomDeck(decks_6),
-        7: getRandomDeck(decks_7),
-        8: getRandomDeck(decks_8),
-        9: getRandomDeck(decks_9),
-        10: getRandomDeck(decks_10),
-        11: getRandomDeck(decks_11),
+        1: getRandomEntry(decks_1),
+        2: getRandomEntry(decks_2),
+        3: getRandomEntry(decks_3),
+        4: getRandomEntry(decks_4),
+        5: getRandomEntry(decks_5),
+        6: getRandomEntry(decks_6),
+        7: getRandomEntry(decks_7),
+        8: getRandomEntry(decks_8),
+        9: getRandomEntry(decks_9),
+        10: getRandomEntry(decks_10),
+        11: getRandomEntry(decks_11),
     };
 
     // Display the response
-    var responseDiv = document.getElementById('response');
+    var responseDiv = document.getElementById('deck_response');
     if (responses.hasOwnProperty(inputNumber)) {
         responseDiv.textContent = responses[inputNumber];
     } else {
         responseDiv.textContent = 'No deck found.';
     }
+});
+
+
+document.getElementById('effectForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    response = getRandomEntry(effects);
+    // Display the response
+    var responseDiv = document.getElementById('effect_response');
+    responseDiv.textContent = response;
+
 });
